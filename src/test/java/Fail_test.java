9bd65c1c46807847890cf077ddc1a894b027ff5f
H4ckerM4nn <sebaste@hiof.no>
2021-11-01 15:48:31 +0100

    Hele prosjektet

---
+++ src/test/java/Fail_test.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class Fail_test {
+
+    @Test
+    public void isLeapYear() {
+
+        fail("we dont have any code here");
+    }
+}
---
+++ src/test/java/isLeapYear.java
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class isLeapYear {
+
+    @ParameterizedTest
+    @CsvSource({
+            "1999, false",
+            "2000, true",
+            "1996, true"
+    })
+    public void DivisibleBy4(int year, boolean output) {
+        assertEquals(output, newLeapYear(year).isLeapYear());
+    }
+
+    private LeapYear newLeapYear(int year) {
+        return new LeapYear(year);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1999, false",
+            "2000, true",
+            "1996, true"
+    })
+    public void DivisibleBy4ButNot100(int input, boolean output) {
+        assertEquals(output, newLeapYear(input).isLeapYear());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1700, false",
+            "1800, false",
+            "2100, false",
+            "1996, true"
+    })
+    public void DivisibleBy4And400ButNot100(int input, boolean output) {
+        assertEquals(output, newLeapYear(input).isLeapYear());
+    }
+
+
+}
